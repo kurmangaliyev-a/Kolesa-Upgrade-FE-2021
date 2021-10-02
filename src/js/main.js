21a80855379f143756cd2f790703c6b653110b0a
kurmangaliyev-a <40716296+kurmangaliyev-a@users.noreply.github.com>
2021-10-02 12:14:42 +0600

    modal window

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,24 @@ document.querySelector('.header .search').addEventListener('click', (e) => {
         document.querySelector('.search input[type="search"]').focus();
     }
 });
+
+function productModalWindow() {
+    const elements = document.querySelectorAll('.product .openModal');
+    const modal = document.querySelector('.modal');
+    const closeModal = document.querySelectorAll('.closeModal');
+
+    elements.forEach((elm) => {
+        elm.addEventListener('click', () => {
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    closeModal.forEach((elm) => {
+        elm.addEventListener('click', () => {
+            modal.classList.add('hide');
+            document.body.style.overflow = '';
+        });
+    });
+}
+
+productModalWindow();
