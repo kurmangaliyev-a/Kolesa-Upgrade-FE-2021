3a739eec64c0b369bd880fb0d05b782ec34cdfc5
kurmangaliyev-a <40716296+kurmangaliyev-a@users.noreply.github.com>
2021-10-02 09:53:59 +0600

    header, banner, actions on main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-import '../css/style.css';
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello, Azamat!</h1>
-`;
+document.querySelector('.header .search').addEventListener('click', (e) => {
+    // if(e.target)
+    if (!e.target.classList.contains('search-icon')) {
+        document.querySelector('.search input[type="search"]').focus();
+    }
+});
