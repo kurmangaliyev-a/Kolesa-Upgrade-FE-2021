691b3e929b7a0fdcf335378e5188b67560ab48c6
kurmangaliyev-a <40716296+kurmangaliyev-a@users.noreply.github.com>
2021-10-06 04:30:10 +0600

    footer по БЭМу

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
+import '../scss/style.scss';
+
 document.querySelector('.header .search').addEventListener('click', (e) => {
-    // if(e.target)
     if (!e.target.classList.contains('search-icon')) {
         document.querySelector('.search input[type="search"]').focus();
     }
